1b86a6681179c4af17aa7f40397b1301f3b1f419
Isispamz <pucapame@gmail.com>
2025-08-11 01:53:07 -0600

    Update twilio.js

--- twilio.js
+++ twilio.js
@@ -1,13 +1,9 @@
-import twilio from 'twilio';
+const twilio = require('twilio');
 const client = twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
 
-// Llama primero a tu número y luego conecta con el número que te escribió por WhatsApp
-export async function startClickToCall(userNumber, otherNumber) {
+async function startClickToCall(userNumber, otherNumber) {
   const twiml = `<Response><Dial callerId="${process.env.TWILIO_CALLER_ID}"><Number>${otherNumber}</Number></Dial></Response>`;
-
-  await client.calls.create({
-    to: userNumber,
-    from: process.env.TWILIO_CALLER_ID,
-    twiml
-  });
+  await client.calls.create({ to: userNumber, from: process.env.TWILIO_CALLER_ID, twiml });
 }
+
+module.exports = { startClickToCall };
